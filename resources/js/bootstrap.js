e0b17da1a9becc157621394ea535bef74f1ddf95
0x8b612 <semihkeskin333@gmail.com>
2019-11-23 01:59:23 +0300

    auth ui yüzünden ortalık karışmış, edited bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -29,7 +29,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001'
+});
 
 // window.Pusher = require('pusher-js');
 
